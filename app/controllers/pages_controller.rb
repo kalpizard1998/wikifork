dce8a72274f0abe43d25f278b2bf5590695f7a28
kalpizard1998 <kevinalpizar1998gmail.com>
2026-04-27 20:00:37 -0600

    Sign in ready

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
+  allow_unauthenticated_access(only: :home)
+  before_action :resume_session, only: :home
   def home
+
   end
 
   def about
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,14 +8,14 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate_by(params.permit(:email_address, :password))
       start_new_session_for user
-      redirect_to after_authentication_url
+      redirect_to after_authentication_url, notice: "Signed in."
     else
       redirect_to new_session_path, alert: "Try another email address or password."
     end
   end
 
-  def destroy
-    terminate_session
-    redirect_to new_session_path, status: :see_other
-  end
+def destroy
+  terminate_session
+  redirect_to new_session_path, notice: "Signed out", status: :see_other
+end
 end
